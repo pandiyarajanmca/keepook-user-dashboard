eab87df846e4d86abad995ad16318d3498fafab1
Pandiyarajan S <pandiyarajan.tcps@gmail.com>
2019-10-03 22:39:14 +0530

    updated

--- src/app/contracts/contracts-routing.module.ts
+++ src/app/contracts/contracts-routing.module.ts
@@ -1,3 +1,10 @@
+import { EditSingleDraftingComponent } from './edit-single-drafting/edit-single-drafting.component';
+import { UserManagePendingExecutionComponent } from './user-manage-pending-execution/user-manage-pending-execution.component';
+import { UserManageExecutedComponent } from './user-manage-executed/user-manage-executed.component';
+import { UserManageDelegatedComponent } from './user-manage-delegated/user-manage-delegated.component';
+import { EditSinglePendingExecutionComponent } from './edit-single-pending-execution/edit-single-pending-execution.component';
+import { EditSingleExecutedComponent } from './edit-single-executed/edit-single-executed.component';
+import { EditSingleDelegatedComponent } from './edit-single-delegated/edit-single-delegated.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -12,6 +19,15 @@ const routes: Routes = [
   {path:'add-contracts', component:AddContractsComponent},
   {path:'edit-contracts', component:EditContractsComponent},
   {path:'all-contracts', component:UserAllContractsComponent},
+  {path:'edit-sigle-delegate', component:EditSingleDelegatedComponent},
+  {path:'edit-sigle-executed', component:EditSingleExecutedComponent},
+  {path:'edit-sigle-drafting', component:EditSingleDraftingComponent},
+  {path:'edit-sigle-pending-execution', component:EditSinglePendingExecutionComponent},
+  {path:'user-manage-delegated', component:UserManageDelegatedComponent},
+  {path:'user-manage-executed', component:UserManageExecutedComponent},
+  {path:'user-manage-pending-execution', component:UserManagePendingExecutionComponent},
+
+
 ];
 
 @NgModule({
